e59ba0f64cb60d13d8c719e8095c6ad1420dc092
Ricardo Velasco Salazar <ricardo.pro@outlook.com>
2020-09-24 09:34:43 -0500

    creating checkout page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { selectCurrentUser } from './redux/user/user.selector';
 import { createStructuredSelector } from 'reselect';
+import CheckoutPage from './pages/checkout/checkout.component';
 
 
 class App extends Component {
@@ -43,7 +44,8 @@ class App extends Component {
         <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route exact path='/shop/' component={ShopPage} />
+          <Route path='/shop/' component={ShopPage} />
+          <Route exact path='/checkout/' component={CheckoutPage} />
           <Route exact path='/signin/' render={() => this.props.currentUser ? (<Redirect to='/' />) : <SignInAndSignUpPage />} />
         </Switch>
       </div>
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -9,7 +9,8 @@ export const selectCartItems = createSelector(
 export const selectCartHidden = createSelector(
     [selectCart],
     cart => cart.hidden
-)
+);
+
 export const selectCartItemsCount = createSelector(
     [selectCartItems],
     cartItems => cartItems.reduce(
@@ -17,4 +18,13 @@ export const selectCartItemsCount = createSelector(
             accumulatedQuantity + cartItem.quantity,
         0
     )
-)
+);
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce(
+        (accumulatedQuantity, cartItem) =>
+            accumulatedQuantity + cartItem.quantity * cartItem.price,
+        0
+    )
+);
