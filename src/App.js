126615485d8da5214125acc21e65908f28419a1d
Ricardo Velasco Salazar <ricardo.pro@outlook.com>
2020-09-24 09:09:55 -0500

    adding createstructuredselector

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { selectCurrentUser } from './redux/user/user.selector';
+import { createStructuredSelector } from 'reselect';
 
 
 class App extends Component {
@@ -49,8 +51,8 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
